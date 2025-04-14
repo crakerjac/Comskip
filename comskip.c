86aabe7bc433c482333ce20ff73cd8d276d1b914
crakerjac <na@noemail.com>
2025-04-14 11:20:06 -0500

    Add option to ini file to only process specific dir

--- comskip.c
+++ comskip.c
@@ -9217,11 +9217,9 @@ FILE* LoadSettings(int argc, char ** argv)
 
     if (dvr_dir)
     {
-        printf("outputdirname= %s\n", outputdirname);
         printf("inbasename= %s\n", inbasename);
-        printf("cl_work= %s\n", cl_work);
         printf("dvr_dir= %s\n", dvr_dir);
-        if (strstr(outputdirname, dvr_dir) == NULL);
+        if (strstr(inbasename, dvr_dir) == NULL);
         {
             printf("File not located in DVR directory, exiting...\n");
             //exit (0);
